9ccb5c5499c182a8f40fd5b4fbe50862664e4b63
BeMuCa <berk.calabakan@gmail.com>
2023-02-24 23:19:52 +0100

    quick fix

--- FeatureRanking.py
+++ FeatureRanking.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ 
+Script to use the PCA for feature extraction and data visualization.
+
 We have a lot of features, using the xgboost feature of showing the gain per feature gives us a idea which of the features 
 are helpful and which seem useless. To be more sure of the ranking of the features we will use the PCA in this script to
 get the features that contribute the most and then try to visualize the 3D of the features and samples.
-- PCA for feature extraction
-- PCA for data visualization
+
 """
+__author__ = "Berk Calabakan"
+
 from wettbewerb import load_references
 from features_112 import features
 from sklearn.decomposition import PCA
-from sklearn.datasets import load_breast_cancer
 import numpy as np
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 # load the features and labels
 ecg_leads,ecg_labels,fs,ecg_names = load_references()
